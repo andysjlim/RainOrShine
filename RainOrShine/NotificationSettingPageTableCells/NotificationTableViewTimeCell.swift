2d983ed3fe4e8d4248b263d83a62dbd683057139
Andy Lim <andylim@Andys-MBP.fios-router.home>
2021-08-30 20:40:56 -0400

    Now saves the time when the user wants the notifications

--- RainOrShine/NotificationSettingPageTableCells/NotificationTableViewTimeCell.swift
+++ RainOrShine/NotificationSettingPageTableCells/NotificationTableViewTimeCell.swift
@@ -11,16 +11,59 @@ import UIKit
 class NotificationTableViewTimeCell: UITableViewCell {
     @IBOutlet weak var timePicker: UIDatePicker!
     
+    var scheduledTimeCD : ScheduledTimeCD?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        {
+            if let savedSchedulesFromCoreData = try? context.fetch(ScheduledTimeCD.fetchRequest()){
+                if let scheduledTimes = savedSchedulesFromCoreData as? [ScheduledTimeCD] {
+                    if scheduledTimes.count != 0 {
+                        scheduledTimeCD = scheduledTimes[0]
+                    }
+                }
+            }
+        }
+        
+        if let nonNullScheduledTimeCD = scheduledTimeCD as ScheduledTimeCD? {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "HH:mm"
+            
+            let date = dateFormatter.date(from: ""+String(nonNullScheduledTimeCD.hour)+":"+String(nonNullScheduledTimeCD.minutes))
+            
+            timePicker.date = date!
+        }
     }
     
-    @IBAction func timeValueChanged(_ sender: UIDatePicker) {
+    @IBAction func timeValueFinishedChanging(_ sender: UIDatePicker) {
         print(sender.date)
         let component = Calendar.current.dateComponents([.hour, .minute], from: sender.date)
         print(component.hour!)
         print(component.minute!)
+        print("User finished changing time")
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        {
+            if let savedSchedulesFromCoreData = try? context.fetch(ScheduledTimeCD.fetchRequest()){
+                if let scheduledTimes = savedSchedulesFromCoreData as? [ScheduledTimeCD] {
+                    let scheduledTimeCD : ScheduledTimeCD
+                    if scheduledTimes.count != 0 {
+                        scheduledTimeCD = scheduledTimes[0]
+                    }
+                    else {
+                        scheduledTimeCD = ScheduledTimeCD(context: context)
+                    }
+                    
+                    scheduledTimeCD.setValue(Int16(component.hour!), forKey: "hour")
+                    scheduledTimeCD.setValue(Int16(component.minute!), forKey: "minutes")
+                    
+                    (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                }
+            }
+        }
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
